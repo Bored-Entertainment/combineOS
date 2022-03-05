49fcf037aff2e4e170009100af0a6d4a5edbcadb
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2022-03-05 14:31:46 -0600

    Added FBF

--- src/programs.js
+++ src/programs.js
@@ -1764,6 +1764,21 @@ add_icon_not_via_filesystem({
     shortcut: true,
 });
 add_icon_not_via_filesystem({
+    title: "Factory Balls Forever",
+    iconID: "fbf",
+    open: function() {
+        Task(make_iframe_window({
+            src: "/games/html/fbf/",
+            icons: iconsAtTwoSizes("fbf"),
+            title: "Factory Balls Forever",
+            outerWidth: 835,
+            outerHeight: 600,
+            resizable: true,
+        }));
+    },
+    shortcut: true,
+});
+add_icon_not_via_filesystem({
     title: "View on GitHub",
     iconID: "github",
     open: function() {
